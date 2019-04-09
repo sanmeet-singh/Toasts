5bd3ee48fabd0076a951af09238604201db69c4d
Sanmeet <apple@Apples-MBP.Dlink>
2019-04-09 14:13:58 +0530

    Added Toast script to do disappear text after sometime

---
+++ Toasts/Assets/Scripts/Toast.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnityToasts
+{
+    public class Toast : MonoBehaviour
+    {
+        private GameObject toastName;
+        private GameObject toastBG;
+
+        private bool animate;
+
+        internal void StartAnimation(GameObject toastName, GameObject toastBG)
+        {
+            this.toastName = toastName;
+            this.toastBG = toastBG;
+
+            animate = true;
+        }
+
+        void Update()
+        {
+            if (animate)
+            {
+                Image image = this.toastBG.GetComponent<Image>();
+                image.color = new Color(image.color.r, image.color.g, image.color.b, (image.color.a - 0.01f));
+
+                Text text = this.toastName.GetComponent<Text>();
+                text.color = new Color(text.color.r, text.color.g, text.color.b, (text.color.a - 0.01f));
+
+                if (image.color.a <= 0)
+                {
+                    animate = false;
+                    Destroy(this.gameObject);
+                }
+            }
+        }
+    }
+}
--- Toasts/Assets/Scripts/Toasts.cs
+++ Toasts/Assets/Scripts/Toasts.cs
@@ -9,17 +9,54 @@ namespace UnityToasts
     {
         public const string CANVAS_NAME = "ToastsCanvas";
         public const string TOASTS_NAME = "Toast";
+        public const string TOASTS_TEXT = "ToastText";
+        public const string TOASTS_BG = "ToastBG";
         public const string DEFAULT_FONT_NAME = "Arial.ttf";
 
         public static void CreateToast(string displayText)
         {
-            GameObject textGO = CreateText(CreateCanvas().transform, displayText);
+            Transform canvas = CreateCanvas().transform;
+
+            GameObject parentGO = CreateToastGameObject(canvas);
+
+            GameObject backgroundGO = CreateBG(parentGO.transform);
+            GameObject textGO = CreateText(parentGO.transform, displayText);
+
+            Toast toast = parentGO.AddComponent<Toast>();
+            toast.StartAnimation(textGO, backgroundGO);
+        }
+
+        private static GameObject CreateToastGameObject(Transform parent)
+        {
+            GameObject toastGO = new GameObject();
+            toastGO.name = TOASTS_NAME;
+            toastGO.transform.parent = parent;
+
+            RectTransform rectTransform = toastGO.AddComponent<RectTransform>();
+            rectTransform.localPosition = new Vector3(0, 0, 0);
+
+            return toastGO;
+        }
+
+        private static GameObject CreateBG(Transform parent)
+        {
+            GameObject backgroundGO = new GameObject();
+            backgroundGO.name = TOASTS_BG;
+            backgroundGO.transform.parent = parent;
+
+            RectTransform rectTransform = backgroundGO.AddComponent<RectTransform>();
+            rectTransform.localPosition = new Vector3(0, 0, 0);
+
+            Image image = backgroundGO.AddComponent<Image>();
+            image.color = Color.red;
+
+            return backgroundGO;
         }
 
         private static GameObject CreateText(Transform parent, string displayText)
         {
             GameObject textGO = new GameObject();
-            textGO.name = TOASTS_NAME;
+            textGO.name = TOASTS_TEXT;
             textGO.transform.parent = parent;
 
             RectTransform rectTransform = textGO.AddComponent<RectTransform>();
